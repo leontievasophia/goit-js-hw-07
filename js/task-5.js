d75a74cd970814e0650dab971be4eca887799e64
leontievasophia <leontievasophia@gmail.com>
2026-06-05 15:59:25 +0300

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const backgroundColor=document.querySelector("body");
+const but=backgroundColor.querySelector(".change-color");
+const spTxt=backgroundColor.querySelector(".color");
+but.addEventListener("click", e=>{
+   const color = getRandomHexColor();
+
+  backgroundColor.style.backgroundColor = color;
+  spTxt.textContent = color;
+
+});
